1160bfc67c547223bd16f9ec1acd28ab36f4f026
kaylasooman <68572668+kaylasooman@users.noreply.github.com>
2020-08-01 13:25:26 -0500

    Update modifyroom.h

--- modifyroom.h
+++ modifyroom.h
@@ -1,10 +1,26 @@
 #include 
 using namespace std
 
-class hotel
-{
-private 
-int_Roomnumber
-int_Capaciy 
-int_Status 
-int_Rate
+class HotelRoom
+{         
+    private:
+        int room_num;
+        int room_capacity;
+        int occupancy;
+       
+    public:
+        HotelRoom();
+        HotelRoom(int, int, int);
+        HotelRoom (const HotelRoom &myroom);
+        HotelRoom& operator=(const HotelRoom &);
+        ~HotelRoom();
+        int getroom_num();
+        int getroom_capacity();
+        int get_occupany();
+        double getdaily_rate();
+        void setroom_num (int);
+        void setroom_capacity(int);
+        void set_occupancy(int);
+        
+        void print();
+};
