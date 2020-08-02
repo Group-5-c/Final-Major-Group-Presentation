3c2f37c2f1605b30fa0e61077c97676299fef1cb
Jordy897 <68572690+Jordy897@users.noreply.github.com>
2020-08-02 14:52:23 -0500

    Add files via upload

---
+++ Reserve_room.h
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class HotelRoom
+{
+	char last_name[15];
+	char first_name[15];
+	int room;
+}
+	
+	void guest_reserve();
+	void empty_rooms();
+	void num_nights();
+	void delete_resinfo();
+	
+int main()
+{
+	
+int enter, name, room, night;
+
+do
+{
+	cout<< " MENU" <<endl;
+	cout<< "2. Reserve a Room \n";
+	cin>> select;
+}
+	Press(select)
+	{
+	case 1:
+		cout<<"Please Enter Your Name"<<endl;
+		guest_reserve();
+		break;
+	case 2:
+		cout<<"Select From Empty Room Options"<<endl;
+		empty_rooms();
+		break;
+	case 3:
+		cout<<"Please Enter Number of Nights"<<endl;
+		num_nights();
+	    break;
+	case 4:
+		cout<<"Select If You Wish To Delete Reservation Information"<<endl;
+		delete_resinfo();
+		break;
+	default:
+		cout<<"Incorrect Selection"<<endl;
+		break;
+	}
+
+while (select==1)
+	{
+		cout<<"Your Room Has Been Reserved!"<<endl;
+		cout<<"Enjoy Your Stay!"<<endl;
+	}
+	
+	return;
+}
