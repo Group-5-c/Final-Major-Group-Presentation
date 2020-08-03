61b0a610bb0c56e44d0ea77e239f773f290a4320
Otilea <65091755+Otilea@users.noreply.github.com>
2020-08-03 00:45:54 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
 
 int select, night;
-char room;
+char room, date;
 double r_rate;
     
 do
@@ -42,40 +42,74 @@ do
                 cout<< "Incorrect option"<<endl;
                 break;
         }
-    
-    cout<< "ROOM \t\t"   <<"CAPACITY \t" <<"ROOM RATE:Peak-Season \t" << "ROOM RATE: Non-Peak Season" <<endl;
-    cout<< "Single \t\t" <<"One (1) \t"  << "$150 \t\t\t"  <<"$120" <<endl;
-    cout<< "Double \t\t" << "Two (2) \t" <<"$175 \t\t\t"   << "$150" <<endl;
-    cout<< "Family \t\t" << "Four (4) \t" << "$200 \t\t\t" << "$175" <<endl <<endl;
-
+    cout<< "ROOM \t\t"   <<"CAPACITY \t" <<"ROOM RATE:Peak-Season (Feb-Jul) \t" << "ROOM RATE: Non-Peak Season (Aug-Dec)" <<endl;
+    cout<< "Single \t\t" <<"One (1) \t"  << "$150 \t\t\t\t\t"  <<"$120" <<endl;
+    cout<< "Double \t\t" << "Two (2) \t" <<"$175 \t\t\t\t\t"   << "$150" <<endl;
+    cout<< "Family \t\t" << "Four (4) \t" << "$200 \t\t\t\t\t" << "$175" <<endl <<endl;
 
         while (select == 1)
         {
             cout<< "Please select room type: \n 's' - single\n 'd' - double \n 'f' - family " <<endl <<endl;
             cin>> room;
-            cout<< "Please enter the number of nights: ";
+            cout<< "Please enter the number of nights: \t";
             cin>> night;
+            cout << "Please enter season of stay(P or NP): \t ";
+            cin >>date;
+
             
             switch (room)
              {
                  case 's':
                 {
-                    r_rate = night*150;
+                     if (date == 'P' || date == 'p')
+                    {
+                       r_rate = night*150;
+                    }
+                    else if (date=='NP' || date=='np'|| date=='Np'|| date=='nP'|| date=='N' || date=='n')
+                    {
+                        r_rate = night*120;
+                    }
+                    else
+                    {
+                        cout<< "Incorrect input" <<endl;
+                    }
                     cout<<"You selected single room and will be spending "<<night <<" night(s)" <<" at a cost of $" <<r_rate <<endl;
-                    cout<< "Thank you for selecting this room. You may continue and reserve this room" <<endl <<endl;
+                    cout<<"Thank you for selecting this room. You may continue and reserve this room" <<endl <<endl;
                     break;
                 }
 
                 case'd':
-                {
-                    r_rate = night*175;
+                {   
+                    if (date == 'P' || date == 'p')
+                    {
+                       r_rate = night*175;
+                    }
+                    else if (date=='NP' || date=='np'|| date=='Np'|| date=='nP'|| date=='N' || date=='n')
+                    {
+                        r_rate = night*150;
+                    }
+                    else
+                    {
+                        cout<< "Incorrect input" <<endl;
+                    }
                     cout<<"You selected double room and will be spending "<<night <<" night(s)" <<" at a cost of $" <<r_rate <<endl;
                     cout<< "Thank you for selecting this room. You may continue and reserve this room" <<endl <<endl;
                     break;
                 }
                 case 'f':
                 {
-                    r_rate = night*200;
+                    if (date=='P' || date=='p')
+                    {
+                       r_rate = night*200;
+                    }
+                    else if (date=='NP' || date=='np'|| date=='Np'|| date=='nP'|| date=='N' || date=='n')
+                    {
+                        r_rate = night*175;
+                    }
+                    else
+                    {
+                        cout<< "Incorrect input" <<endl;
+                    }
                     cout<<"You have selected family which accomdates up to four(4) persons and will be spending "<<night <<" night(s)" <<" at a cost of $" <<r_rate <<endl;
                     cout<< "Thank you for selecting this room. You may continue and reserve this room" <<endl <<endl;
                     break;
