696353ba0148347b3b64790697bd75bfe7b9c419
kaylasooman <68572668+kaylasooman@users.noreply.github.com>
2020-08-03 14:18:22 -0500

    Update modifyroom.h

--- modifyroom.h
+++ modifyroom.h
@@ -1,24 +1,48 @@
 #include<iostream>
 using namespace std;
 
-class Hotelroom;
+
+ {
+	private:
+		int room_num;
+		int room_capacity;
+		int room_occupancy;
+		int room_status;
+		int room_rate;
+		
+	public:
+		Hotelroom();
+		int getroom_num();
+		int getroom_capacity();
+		int getroom_occupancy();
+		int getroom_status();
+		int getroom_rate();
+		
+	{
+		int_nonpeak=150
+		int_peak=175
+		
+	}
+
+
 {
-    private:
-    int room_num;
-    int room_capacity;
-    int occupancy;
-    int status;
-    int rate;
-    
-    public:
-    Hotelroom();
-    int getroom_num();
-    int getroom_capacity(); 
-    int get_occupancy();
-    int get_status();
-    int get_rate();
-    double getdaily_rate();
-    void setroom_num (int);
-    void setroom_capacity(int);
-    void set_occupancy(int);
-};
+	cout<< " MENU" <<endl;
+	cout<< "3. Modify a Room \n";
+	cin>> select;
+}
+	Press(select)
+	{
+		cout<<"Enter the number of guest to occupy room"<<endl;
+		guest_occupy();
+		break;
+		
+	}
+
+while (select==1)
+	
+		cout<<"Your Room Has Been Reserved!"<<endl;
+		cout<<"Enjoy Your Stay!"<<endl;
+	
+	
+	return;
+}
